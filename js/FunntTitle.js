d0d0b3fa86e2c321597f4cc3e88ed25b7f731a6a
左嘉辉 <zuojiahui@zuojiahuideMacBook-Pro.local>
2021-11-01 23:01:53 +0800

    Site updated: 2021-11-01 23:01:52

---
+++ js/FunntTitle.js
@@ -0,0 +1,17 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = '╭(°A°`)╮ 😥喔呦，崩溃啦 ~';
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = '(ฅ>ω<*ฅ)😊咦,又好啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
